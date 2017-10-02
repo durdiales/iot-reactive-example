142bc29d1d47bd03f2318842cd46f6206fd0636e
José Antonio Zumaquero Torres <jazumaquero@gmail.com>
2017-10-02 15:42:47 +0200

    Fixed service locator not found.
    It was needed to override method on loader in order to connect to local
    dev service locator.

--- datalogger-impl/src/main/scala/com/example/datalogger/impl/DataLoggerLoader.scala
+++ datalogger-impl/src/main/scala/com/example/datalogger/impl/DataLoggerLoader.scala
@@ -3,7 +3,8 @@ package com.example.datalogger.impl
 import com.example.datalogger.api.DataLoggerService
 import com.lightbend.lagom.scaladsl.api.ServiceLocator
 import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaComponents
-import com.lightbend.lagom.scaladsl.server.{LagomApplicationContext, LagomApplicationLoader}
+import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
+import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomApplicationLoader}
 
 /**
   * Loads the implementation of the data logger api defined on {@link DataLoggerService} trait.
@@ -16,6 +17,9 @@ class DataLoggerLoader extends LagomApplicationLoader {
     new DataLoggerApplication(context) with LagomKafkaComponents {
       override def serviceLocator = ServiceLocator.NoServiceLocator
     }
+
+  override def loadDevMode(context: LagomApplicationContext): LagomApplication =
+    new DataLoggerApplication(context) with LagomDevModeComponents  with LagomKafkaComponents
 
   override def describeService = Some(readDescriptor[DataLoggerService])
 }
